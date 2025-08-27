2a4c91c01e8448b3fba71446596b29dfaf328cca
Ridwan Saefudin <ridwansaefudin97@gmail.com>
2025-08-27 16:27:40 +0700

    add nacos-mysql packages (LFS)

---
+++ nacos-mysql/nacos-mysql-fixed.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cfcfac521a408c6235c56cb19270387b443c310ef76029fff3b36a28b17f2da9
+size 4874
---
+++ nacos-mysql/nacos-mysql.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:97a76b900ed4a0220f476a9b5e1f4eeb06aa5e746137238cb76e15c39243d3a7
+size 4874
